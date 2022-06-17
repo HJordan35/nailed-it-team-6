3d99b2bebf3df4b9a262fefb21dd7d9e955c76a5
Henry Jordan <henry.jordan@credera.com>
2022-06-17 10:04:09 -0500

    ADD | preliminary implementation of jackpot counter

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import "./App.css";
 import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import ArcadeBackgroundUnlit from "../src/assets/ArcadeBackgroundUnlit.svg";
+import JackpotCounter from "./components/jackpot-counter";
 
-function App() {
+const App = () => {
   return (
     <div className="app">
       <nav className="app-nav">
-        <img className="app-nav-image" src={ArcadeBackgroundUnlit}/>
+        <img className="app-nav-image" src={ArcadeBackgroundUnlit} />
         {/*
         <Link to="/">Home</Link>
         <Link to="objective">Objective</Link>
         <Link to="teams">Teams</Link>
         <Link to="thanks">Thank You</Link> */}
       </nav>
+      <JackpotCounter />
       <div className="app-content">
         <Outlet />
       </div>
@@ -22,6 +24,6 @@ function App() {
       </footer>
     </div>
   );
-}
+};
 
 export default App;
