211112c66558d1b9bbe8f94f9460d0b61c52ea6d
Tony Kim <tony.kim@credera.com>
2022-06-17 09:46:28 -0500

    adding svg nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import "./App.css";
 import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
+import ArcadeBackgroundUnlit from "../src/assets/ArcadeBackgroundUnlit.svg";
 
 function App() {
   return (
     <div className="app">
       <nav className="app-nav">
+        <img className="app-nav-image" src={ArcadeBackgroundUnlit}/>
+        {/*
         <Link to="/">Home</Link>
         <Link to="objective">Objective</Link>
         <Link to="teams">Teams</Link>
-        <Link to="thanks">Thank You</Link>
+        <Link to="thanks">Thank You</Link> */}
       </nav>
       <div className="app-content">
         <Outlet />
